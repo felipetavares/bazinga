db38b6a63ef6115e25ffb134e2c5b9af999ebcf1
CCSL-IFRN <felipe.oltavares@gmail.com>
2015-02-19 20:17:01 -0300

    It's compilling under linux

--- gl/glwin.cpp
+++ gl/glwin.cpp
@@ -1,5 +1,10 @@
 #include "glwin.h"
+#ifdef _WIN32
 #include <windows.h>
+#else
+#include <GL/glx.h>
+//extern void* glXGetProcAddress(const GLubyte *);
+#endif
 #include <iostream>
 using namespace std;
 
@@ -12,9 +17,15 @@ void *getGL(const char *name) {
     HMODULE module = LoadLibraryA("opengl32.dll");
     p = (void *)GetProcAddress(module, name);
   }
- 
+
   return p;
 }
+#else
+void *getGL(const char *name) {
+  void *p = (void *)glXGetProcAddress((GLubyte *)name);
+  return p;
+}
+#endif
 
 PFNGLCREATEPROGRAMPROC    glCreateProgram;
 PFNGLDELETEPROGRAMPROC	  glDeleteProgram;
@@ -29,35 +40,32 @@ PFNGLLINKPROGRAMPROC 	  glLinkProgram;
 PFNGLGETPROGRAMIVPROC	  glGetProgramiv;
 PFNGLGETSHADERINFOLOGPROC glGetProgramInfoLog;
 PFNGLUSEPROGRAMPROC		  glUseProgram;
-PFNGLACTIVETEXTUREPROC	  glActiveTexture;
+//PFNGLACTIVETEXTUREPROC	  glActiveTexture;
 PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
 PFNGLUNIFORM1IPROC		  glUniform1i;
 PFNGLUNIFORM4FPROC		  glUniform4f;
 
 void initGL() {
-	cout << "bazinga: opengl: doing some work Lazy Windows can't do by itself" << endl;
-	cout << "\t" << "loading OpenGL function definitions" << endl;
-
-	glCreateProgram = (PFNGLCREATEPROGRAMPROC)getGL("glCreateProgram");
-	glDeleteProgram = (PFNGLDELETEPROGRAMPROC)getGL("glDeleteProgram");
-	glCreateShader = (PFNGLCREATESHADERPROC)getGL("glCreateShader");
-	glShaderSource = (PFNGLSHADERSOURCEPROC)getGL("glShaderSource");
-	glCompileShader = (PFNGLCOMPILESHADERPROC)getGL("glCompileShader");
-	glGetShaderiv = (PFNGLGETSHADERIVPROC)getGL("glGetShaderiv");
-	glDeleteShader = (PFNGLDELETESHADERPROC)getGL("glDeleteShader");
-	glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)getGL("glGetShaderInfoLog");
-	glAttachShader = (PFNGLATTACHSHADERPROC)getGL("glAttachShader");
-	glLinkProgram = (PFNGLLINKPROGRAMPROC)getGL("glLinkProgram");
-	glGetProgramiv = (PFNGLGETPROGRAMIVPROC)getGL("glGetProgramiv");
-	glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)getGL("glGetProgramInfoLog");
-	glUseProgram = (PFNGLUSEPROGRAMPROC)getGL("glUseProgram");
-	glActiveTexture = (PFNGLACTIVETEXTUREPROC)getGL("glActiveTexture");
-	glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)getGL("glGetUniformLocation");
-	glUniform1i = (PFNGLUNIFORM1IPROC)getGL("glUniform1i");
-	glUniform4f = (PFNGLUNIFORM4FPROC)getGL("glUniform4f");
-}
-#else
-void initGL() {
-
-}
+#ifdef _WIN32
+  cout << "bazinga: opengl: doing some work Lazy Windows can't do by itself" << endl;
 #endif
+  cout << "\t" << "loading OpenGL function definitions" << endl;
+
+  glCreateProgram = (PFNGLCREATEPROGRAMPROC)getGL("glCreateProgram");
+  glDeleteProgram = (PFNGLDELETEPROGRAMPROC)getGL("glDeleteProgram");
+  glCreateShader = (PFNGLCREATESHADERPROC)getGL("glCreateShader");
+  glShaderSource = (PFNGLSHADERSOURCEPROC)getGL("glShaderSource");
+  glCompileShader = (PFNGLCOMPILESHADERPROC)getGL("glCompileShader");
+  glGetShaderiv = (PFNGLGETSHADERIVPROC)getGL("glGetShaderiv");
+  glDeleteShader = (PFNGLDELETESHADERPROC)getGL("glDeleteShader");
+  glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)getGL("glGetShaderInfoLog");
+  glAttachShader = (PFNGLATTACHSHADERPROC)getGL("glAttachShader");
+  glLinkProgram = (PFNGLLINKPROGRAMPROC)getGL("glLinkProgram");
+  glGetProgramiv = (PFNGLGETPROGRAMIVPROC)getGL("glGetProgramiv");
+  glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)getGL("glGetProgramInfoLog");
+  glUseProgram = (PFNGLUSEPROGRAMPROC)getGL("glUseProgram");
+  //glActiveTexture = (PFNGLACTIVETEXTUREPROC)getGL("glActiveTexture");
+  glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)getGL("glGetUniformLocation");
+  glUniform1i = (PFNGLUNIFORM1IPROC)getGL("glUniform1i");
+  glUniform4f = (PFNGLUNIFORM4FPROC)getGL("glUniform4f");
+}
